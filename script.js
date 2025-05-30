e7a13acd1cd626b4df092df312c8746faf48a86c
Samuel dos Santos Batista <samuca124656@gmail.com>
2025-05-30 20:03:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function saudacao(){
+  console.log("Olá, Seja bem vindo!")
+
+}
+saudacao();
+function apresentar(nome){
+    console.log(`Olá, Seja bem vindo ${nome}!`);
+}
+    apresentar("Samuel");
+
